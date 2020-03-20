31e1120c50459b809814a405cb6da2c24cf38792
Daven Quinn <dev@davenquinn.com>
2020-03-20 01:56:23 -0500

    Added pipeline files to lab-specific repository

---
+++ plugins/cloud_data.py
@@ -0,0 +1,4 @@
+from sparrow.plugins.cloud_data import CloudDataPlugin
+
+class LaserChronDataPlugin(CloudDataPlugin):
+    name = "laserchron-data"
--- sparrow-config.sh
+++ sparrow-config.sh
@@ -1,19 +1,21 @@
 unset GREP_OPTIONS
 # Configures environment for LaserChron lab
-
 PROJECT_DIR="${0:A:h}"
+
 export SPARROW_BACKUP_DIR="$PROJECT_DIR/database-backups"
 export SPARROW_PATH="$PROJECT_DIR/Sparrow"
 export SPARROW_LAB_NAME="Arizona LaserChron Center"
 export COMPOSE_PROJECT_NAME="laserchron"
 # Need to figure out a better way to do this, but it'll be finicky
 # because of docker container nonsense
-pipeline=$PROJECT_DIR/Sparrow/import-pipelines/LaserChron
-export SPARROW_INIT_SQL="$pipeline/sql"
-export SPARROW_COMMANDS="$pipeline/bin"
-export SPARROW_SITE_CONTENT="$pipeline/site-content"
+export SPARROW_INIT_SQL="$PROJECT_DIR/sql"
+export SPARROW_COMMANDS="$PROJECT_DIR/bin"
+export SPARROW_SITE_CONTENT="$PROJECT_DIR/site-content"
 export SPARROW_HTTP_PORT=5002
 
+export SPARROW_PLUGIN_DIR="$PROJECT_DIR/plugins"
+
+# S3 stuff
 export SPARROW_S3_ENDPOINT="https://sfo2.digitaloceanspaces.com"
 export SPARROW_S3_BUCKET="laserchron-data"
 
