39346edcf9c4674bbc88ae56d463ce4fe903abb8
Daven Quinn <dev@davenquinn.com>
2020-05-29 22:01:15 +0000

    Updated configuration

--- sparrow-config.sh
+++ sparrow-config.sh
@@ -2,14 +2,16 @@ unset GREP_OPTIONS
 # Configures environment for LaserChron lab
 
 PROJECT_DIR="/laserchron-sparrow"
-export SPARROW_SITE_CONTENT="$PROJECT_DIR/site-content"
 export SPARROW_BACKUP_DIR="/database-backups"
+export SPARROW_LAB_NAME="Arizona LaserChron Center"
 export SPARROW_SECRET_KEY="GraniteAndesiteBasaltGabbro"
 # Need to figure out a better way to do this, but it'll be finicky
 # because of docker container nonsense
 pipeline=$PROJECT_DIR/Sparrow/import-pipelines/LaserChron
 export SPARROW_INIT_SQL="$pipeline/sql"
 export SPARROW_COMMANDS="$pipeline/bin"
+export SPARROW_SITE_CONTENT="$pipeline/site-content"
+export SPARROW_HTTP_PORT=80
 
 # Needed for import script
 export SPARROW_DATA_DIR="/Data"
