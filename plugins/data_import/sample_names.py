1abdae9f8dea8e5d9f4bcb958057d49e4f7c188c
Daven Quinn <dev@davenquinn.com>
2020-05-29 05:52:24 -0500

    Sample name importing is much improved

--- plugins/data_import/sample_names.py
+++ plugins/data_import/sample_names.py
@@ -6,15 +6,23 @@ import re
 
 from sparrow.import_helpers import SparrowImportError
 
-def extract_analysis_suffix(sample_name):
+def extract_analysis_name(sample_name):
     sep = r"[\.\:_\s-]"
-    pat = sep+r"(Spot )?(\w+)$"
+
+    # We have something in the form "Spot xxx"
+    pat = sep+r"(Spot.+)$"
+    s = re.search(pat, str(sample_name), flags=re.IGNORECASE)
+    if s is not None:
+        return s.group(1)
+
+    # We just have a delimited last word (without a spot)
+    pat = sep+r"(\w+)$"
     try:
         s = re.search(pat, str(sample_name), flags=re.IGNORECASE)
     except TypeError:
         return None
     if s is not None:
-        return s.group(2)
+        return s.group(1)
 
     pat = sep+r"(\d+)$"
     s = re.search(pat, str(sample_name))
@@ -24,59 +32,84 @@ def extract_analysis_suffix(sample_name):
 
 
 def remove_suffix(s1, s2):
-    if s1.endswith(s2):
-        return s1[:-len(s2)]
+    try:
+        if s1.endswith(s2):
+            return s1[:-len(s2)]
+    except (TypeError, AttributeError):
+        pass
     return s1
 
+delimiters = '.:_- '
 
-def strip_analysis_suffix(row):
-    s1 = str(row['analysis'])
-    s2 = str(row['analysis_suffix'])
-    return (remove_suffix(s1, str(s2))
-            .rstrip('.:_- '))
+def strip_analysis_name(row):
+    s1 = row['analysis']
+    s2 = row['analysis_name']
+    return str(remove_suffix(s1, s2)).rstrip(delimiters)
 
-def generalize_samples(data):
+def print_sample_info(df, verbose=False):
+    if verbose:
+        echo(style("Samples: ", bold=True), err=True)
+        for id, group in df.groupby(["sample_id"]):
+            n = style(f" ({len(group)})", dim=True)
+            echo("- "+style(id, fg='cyan')+n, err=True)
+        echo("", err=True)
+    else:
+        echo(style("Samples: ", bold=True)+"   ".join(data.sample_id.unique()), err=True)
+
+def generalize_samples(input):
     """Generalize sample ids into `sample_name`, `analysis_name`,
        and `session_index` columns"""
 
     # Create sample name columns
-    data.reset_index(inplace=True)
+    data = input.reset_index()
     data.rename(columns={'Analysis': 'analysis'}, inplace=True)
 
     # Strip out extra data
-    data['analysis'] = data['analysis'].str.strip()
-    data['analysis_suffix'] = data['analysis'].apply(extract_analysis_suffix)
-    data['sample_id'] = data.apply(strip_analysis_suffix, axis=1)
+    data['analysis'] = data['analysis'].str.strip(delimiters)
+    data['analysis_name'] = data['analysis'].apply(extract_analysis_name)
+    # Strip the analysis suffix off of the sample ID
+    data['sample_id'] = data.apply(strip_analysis_name, axis=1)
 
-    print(data['sample_id'].unique())
+
 
     for ix, group in data.groupby(["sample_id"]):
-        unique_suffix = group['analysis_suffix'].unique()
-        # If we don't have enough unique suffixes, we
-        # fall back to the original sample id
+        unique_suffix = group['analysis_name'].unique()
+        # If we don't have enough unique suffixes, it's probable that we actually
+        # grabbed part of the sample ID. In that case, we fall back to the
+        # original sample id
         _ix = data['sample_id'] == ix
-        if len(unique_suffix)/len(group) < 0.8:
+        if len(unique_suffix)/len(group) < 0.4:
             data.loc[_ix, 'sample_id'] = data.loc[_ix, 'analysis']
+            data.loc[_ix, 'analysis_name'] = None
 
-    print(data)
+        if ix.startswith('Spot'):
+            data.loc[_ix, 'sample_id'] = None
+            data.loc[_ix, 'analysis_name'] = data.loc[_ix, 'analysis']
 
+    n_samples = len(data['sample_id'].unique())
+    if n_samples > 0.3*len(data) and n_samples > 20:
+        # We have a lot of "unique" samples even though we tried to extract
+        # sample IDs. We are probably doing something wrong.
+        raise SparrowImportError("Too many unique samples; skipping import.")
 
-    # Session index should be integer, so we set things that don't match
-    # to NaNs
-    data['session_ix'] = to_numeric(
-        data['analysis_suffix'],
+    cleaned_name = data['analysis_name'].str.replace("Spot", "").str.strip(delimiters)
+
+    # Session index is just inferred by integer order within groups
+    # We don't mess with grain numbers
+    data['session_index'] = to_numeric(
+        cleaned_name,
         errors='coerce',
         downcast='integer')
-    data.drop('analysis_suffix', axis=1, inplace=True)
 
-    echo(style("Samples: ", bold=True)+"   ".join(data.sample_id.unique()))
+    print_sample_info(data, verbose=True)
 
-    return data.set_index(["sample_id", "session_ix"], drop=True)
+    return data.set_index(["sample_id", "analysis_name"], drop=True)
 
 def list_sample_names(data_files, verbose=False):
+    """List sample names found in a set of CSV data tables, for debugging purposes."""
+
     from .laserchron_importer import decode_datatable
 
-    """List sample names found in a set of CSV data tables, for debugging purposes."""
     for i, file in enumerate(data_files):
         print(file.file_path)
         try:
@@ -85,9 +118,7 @@ def list_sample_names(data_files, verbose=False):
                 for line in wrap("  ".join([f"{i:20}" for i in df.index]), 80):
                     print(line)
             df1 = generalize_samples(df)
-            print(df1.index)
-            ##secho("  ".join(df1.index.unique()), fg='cyan')
 
-        except SparrowImportError:
-            secho("Encountered an error!", fg='red')
+        except SparrowImportError as err:
+            secho(str(err), fg='red')
         print("")
